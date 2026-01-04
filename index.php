5bfdb5008c75a8204bd84aa5e2bff597ffe096c2
thaonguyen1231993n-a11y <thaonguyen1231993n@gmail.com>
2026-01-05 01:55:14 +0700

    Update index.php

--- index.php
+++ index.php
@@ -88,20 +88,20 @@
         function displayContent($content) {
             if (empty($content)) return "";
 
-            // 1. Xử lý Link YOUTUBE (Dạng: youtube.com hoặc youtu.be)
+            // 1. Xử lý Link YOUTUBE
+            // Thêm style width:100%; height:100%
             $content = preg_replace(
                 '/(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([a-zA-Z0-9_-]{11})([^\s<]*)/', 
-                '<div class="video-responsive"><iframe src="https://www.youtube.com/embed/$1" allowfullscreen></iframe></div>', 
+                '<div class="video-responsive"><iframe src="https://www.youtube.com/embed/$1" style="width:100%; height:100%; border:0;" allowfullscreen></iframe></div>', 
                 $content
             );
 
             // 2. Xử lý Link FACEBOOK
-            // Logic: Bỏ width cố định, thêm height=100% để nó bung lụa trong khung
             $content = preg_replace_callback(
                 '/(https?:\/\/(?:www\.|web\.|m\.)?facebook\.com\/(?:watch\/\?v=\d+|[a-zA-Z0-9.]+\/videos\/\d+|reel\/|share\/v\/)[^\s<]*)/',
                 function($matches) {
                     $videoUrl = urlencode($matches[1]);
-                    // Lưu ý: Mình bỏ 'width=560' và thay bằng style width:100%; height:100%
+                    // Set width=100%, height=100% để CSS bên ngoài điều khiển
                     return '<div class="video-responsive">
                                 <iframe src="https://www.facebook.com/plugins/video.php?href=' . $videoUrl . '&show_text=false&t=0" 
                                         style="width:100%; height:100%; border:none; overflow:hidden;" 
@@ -263,6 +263,7 @@
 
 </body>
 </html>
+
 
 
 
